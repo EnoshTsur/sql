73618c505feac8543d1b1811ea0eed9f69138c22
Nimmy The Great <Nimrod@reshef.com>
2020-01-21 20:55:06 +0200

    joins

--- jo.sql
+++ jo.sql
@@ -17,6 +17,46 @@ SELECT my_db.BOOKS.*, my_db.AUTHOR.FIRST_NAME FROM my_db.BOOKS, my_db.AUTHOR whe
 
 SELECT * FROM my_db.BOOKS where my_db.BOOKS.AUTHOR_ID IN ( SELECT ID FROM my_db.AUTHOR WHERE ID = 2 ) 
 
-SELECT * FROM my_db.books INNER JOIN my_db.author ON my_db.books.AUTHOR_ID = my_db.author.ID
+##########################################
+### Examples #############################
+##########################################
+SELECT my_db.BOOKS.* FROM my_db.BOOKS, my_db.AUTHOR 
+where books.AUTHOR_ID = author.ID
 
-## LEFT | RIGHT
+SELECT my_db.BOOKS.name , my_db.books.PRICE FROM my_db.BOOKS , my_db.AUTHOR 
+where ((author.first_name = 'gil' and author.LAST_NAME = 'sasover')
+or (author.FIRST_NAME = 'dana' and author.LAST_NAME = 'dvorin')) and books.AUTHOR_ID = author.ID
+
+select my_db.books.NAME, my_db.books.PRICE from my_db.books where my_db.books.PRICE < 20
+
+SELECT my_db.BOOKS.name , my_db.books.PRICE FROM my_db.BOOKS , my_db.AUTHOR 
+where ((author.first_name = 'efrat' and author.LAST_NAME = 'rayten')
+or (author.FIRST_NAME = 'nimrod' and author.LAST_NAME = 'reshef')) and books.price < 20 and books.AUTHOR_ID = author.ID
+
+
+SELECT name FROM my_db.BOOKS where AUTHOR_ID IN (
+SELECT ID FROM my_db.AUTHOR where first_name = 'gil' and  last_name = 'sasover'
+)
+
+SELECT price FROM my_db.BOOKS where AUTHOR_ID IN (
+SELECT ID FROM my_db.AUTHOR where age < 98
+)
+
+SELECT price FROM my_db.BOOKS where AUTHOR_ID IN (
+SELECT ID FROM my_db.AUTHOR where first_name like 'd%' or first_name like '%l'
+)
+											
+
+SELECT name FROM my_db.BOOKS where AUTHOR_ID IN (
+SELECT ID FROM my_db.AUTHOR where first_name like '%r%'
+)
+##########################################
+### Examples #############################
+##########################################
+
+
+SELECT * FROM my_db.books INNER JOIN my_db.author ON my_db.books.AUTHOR_ID = my_db.author.ID
+SELECT * FROM my_db.books LEFT JOIN my_db.author ON my_db.books.AUTHOR_ID = my_db.author.ID
+SELECT * FROM my_db.books RIGHT JOIN my_db.author ON my_db.books.AUTHOR_ID = my_db.author.ID
+SELECT * FROM my_db.books FULL JOIN my_db.author 
+
